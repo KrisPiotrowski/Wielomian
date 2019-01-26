a892e76eeed18687e3b5a786c529a270dccdd9df
Krzysztof Piotrowski <krzysztof.piotrowski@microsoft.wsei.edu.pl>
2019-01-26 22:29:06 +0100

    Add project files.

---
+++ Wielomian/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wielomian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
